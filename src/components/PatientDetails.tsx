bb396af1e2b26a238d1f9a474adb77afd89dd79e
Leo Arias <larias@alumno.unlam.edu.ar>
2024-09-26 01:28:53 -0300

    first commit

---
+++ src/components/PatientDetails.tsx
@@ -0,0 +1,48 @@
+import { usePatientStore } from "../store";
+import { Patient } from "../types";
+import PatientDetailItem from "./PatientDetailItem";
+import { toast } from "react-toastify";
+
+type PatientDetailsProps = {
+    patient: Patient;
+};
+
+export default function PatientDetails({ patient }: PatientDetailsProps) {
+    const deletePatient = usePatientStore((state) => state.deletePatient);
+    const getPatientById = usePatientStore((state) => state.getPatientById);
+
+    const handleDelete = () => {
+        deletePatient(patient.id);
+        toast.error("Paciente Eliminado");
+    };
+
+    return (
+        <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+            <PatientDetailItem label="ID" data={patient.id} />
+            <PatientDetailItem label="Paciente" data={patient.name} />
+            <PatientDetailItem label="Propietario" data={patient.caretaker} />
+            <PatientDetailItem label="Email" data={patient.email} />
+            <PatientDetailItem
+                label="Fecha Alta"
+                data={patient.date.toString()}
+            />
+            <PatientDetailItem label="Síntomas" data={patient.symptoms} />
+
+            <div className="flex flex-col lg:flex-row gap-3 justify-between mt-10">
+                <button
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                    onClick={() => getPatientById(patient.id)}
+                >
+                    Editar
+                </button>
+
+                <button
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+                    onClick={handleDelete}
+                >
+                    Eliminar
+                </button>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/PatientsList.tsx
@@ -0,0 +1,41 @@
+import { usePatientStore } from "../store";
+import PatientDetails from "./PatientDetails";
+
+export default function PatientsList() {
+    const patients = usePatientStore((state) => state.patients);
+    return (
+        <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll">
+            {patients.length ? (
+                <>
+                    <h2 className="font-black text-3xl text-center">
+                        Lista de Pacientes
+                    </h2>
+
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Administra tus {""}
+                        <span className="text-indigo-600 font-bold">
+                            Pacientes y Citas
+                        </span>
+                    </p>
+
+                    {patients.map((patient) => (
+                        <PatientDetails key={patient.id} patient={patient} />
+                    ))}
+                </>
+            ) : (
+                <>
+                    <h2 className="font-black text-3xl text-center">
+                        No hay Pacientes
+                    </h2>
+
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Comienza añadiendo un Paciente {""}
+                        <span className="text-indigo-600 font-bold">
+                            y aparecerán en este lugar
+                        </span>
+                    </p>
+                </>
+            )}
+        </div>
+    );
+}
